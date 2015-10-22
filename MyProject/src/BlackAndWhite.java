f3cb74aa5886f32311e8479bf473cd0d98ef55ed
Delilah1616 <da716@live.mdx.ac.uk>
2015-10-22 08:49:21 +0100

    I tried to get a black and white output images from this code, but what I got is images with hsv color as gray method did not work, I found the output images in different directory than the one I wanted to be ..  .. I have got the images inside the project directory!

---
+++ MyProject/src/BlackAndWhite.java
@@ -0,0 +1,138 @@
+import java.awt.Image;
+	import java.awt.image.BufferedImage;
+	import java.awt.image.RenderedImage;
+	import java.io.File;
+	import java.io.IOException;
+ 
+	import javax.imageio.ImageIO;
+	import javax.swing.ImageIcon;
+	import javax.swing.JDialog;
+	import javax.swing.JFrame;
+	import javax.swing.JLabel;
+	import javax.swing.JOptionPane;
+
+	import org.opencv.core.Core;
+	import org.opencv.core.Mat;
+	import org.opencv.core.MatOfRect;
+	import org.opencv.core.Point;
+	import org.opencv.core.Rect;
+	import org.opencv.core.Scalar;
+	import org.opencv.core.Size;
+	import org.opencv.imgcodecs.Imgcodecs;
+	import org.opencv.imgproc.Imgproc;
+	import org.opencv.objdetect.CascadeClassifier;
+
+
+import org.opencv.core.CvType;
+
+
+
+	import java.awt.Image;
+	import java.awt.image.BufferedImage;
+	import java.io.File;
+	import java.io.IOException;
+	import java.nio.file.DirectoryStream;
+	import java.nio.file.Files;
+	import java.nio.file.Path;
+	import java.nio.file.Paths;
+
+	import javax.imageio.ImageIO;
+	import javax.imageio.stream.ImageOutputStream;
+	import javax.swing.ImageIcon;
+	import javax.swing.JFrame;
+	import javax.swing.JLabel;
+
+	import org.opencv.core.Core;
+	import org.opencv.core.Mat;
+	import org.opencv.core.MatOfRect;
+	import org.opencv.core.Point;
+	import org.opencv.core.Rect;
+	import org.opencv.core.Scalar;
+	import org.opencv.imgcodecs.Imgcodecs;
+	import org.opencv.imgproc.Imgproc;
+	import org.opencv.objdetect.CascadeClassifier;
+	
+	import java.awt.image.BufferedImage;
+	import java.awt.image.DataBufferByte;
+
+	import java.io.File;
+	import javax.imageio.ImageIO;
+
+
+	public class BlackAndWhite {
+
+
+		public void run() {
+
+			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+			File folder = new File("/Users/Delilah/Documents/finalproject/testdalal");
+			File[] listOfFiles = folder.listFiles();
+			
+			File folderBW = new File("/Users/Delilah/Documents/finalproject/testdalal/TEST/BlackAndWhite/");
+			File[] listOfFilesBW = folderBW.listFiles();
+			
+ 
+
+			
+			for (int i = 0; i < listOfFiles.length; i++){
+				 
+				String picname = (listOfFiles[i]).getName(); //filename in input folder
+				
+				String bwPath = "/Users/Delilah/Documents/finalproject/testdalal/"; //input folder
+			 
+				String picPathName = bwPath + picname; //pic filename is the foldername  and the filename
+				 
+				File input = new File(picPathName);
+		        BufferedImage image;
+				try {
+					image = ImageIO.read(input); //read the image
+					
+			         byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+					 Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3); //create mat using the heigh and width of the image
+			         mat.put(0, 0, data);
+			         Mat mat1 = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3); //creates mat1, grayscaled image
+			     	
+			         Imgproc.cvtColor(mat, mat1, Imgproc.COLOR_RGB2HSV); //take mat and changes it into mat1 OF GRAYCOLOR
+			         //Imgproc.cvtColor(mat, mat1, Imgproc.COLOR_RGB2GRAY);
+			         
+					 String fileName = listOfFiles[i].getName()+"-bw.jpg";	
+					 //System.out.println("name of output path and file name " + folderBW +"/"+fileName+"-bw.jpg"); //bw.jpg to the filename and print it
+					 
+					 byte[] data1 = new byte[mat1.rows()*mat1.cols()*(int)(mat1.elemSize())];
+			         mat1.get(0, 0, data1);
+			         BufferedImage image1 = new BufferedImage(mat1.cols(), mat1.rows(), 5);
+			         image1.getRaster().setDataElements(0, 0, mat1.cols(), mat1.rows(), data1); //builds the output image 
+ 
+			         File ouptut = new File(fileName);
+			        // File ouptut = new File("/Users/Delilah/Documents/finalproject/testdalal/TEST/BlackAndWhite2/"+fileName);
+			         ImageIO.write(image1, "jpg", ouptut); //outputs image 1 writes it to the folder
+					
+					
+					
+					
+					
+					
+					Imgcodecs.imwrite("/Users/Delilah/Documents/finalproject/testdalal/TEST/BlackAndWhite/"+fileName+"-bw.jpg", mat1);
+			
+				} catch (IOException e) {
+					System.out.println("cant read image");
+					e.printStackTrace();
+				}
+			}//for all files in the folder
+
+			 
+			
+		}//run
+
+ 
+		public static void main (String[] args) {
+			BlackAndWhite fd = new BlackAndWhite();
+			fd.run();
+		}
+	}
+
+
+
+	
+        
