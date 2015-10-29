0f8be47f131ead7ae93dbda1c5e8831e1088849f
Delilah1616 <da716@live.mdx.ac.uk>
2015-10-29 12:09:32 +0000

    test comment

--- MyProject/src/BlackAndWhite.java
+++ MyProject/src/BlackAndWhite.java
@@ -2,9 +2,12 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
-	import javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -12,7 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import org.omg.CORBA.OMGVMCID;
-	import org.opencv.core.Core;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
@@ -24,10 +27,7 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.core.CvType;
 
-
-
-
-	import java.awt.Image;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -36,13 +36,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-	import javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageOutputStream;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-	import org.opencv.core.Core;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
@@ -52,115 +52,93 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
-	import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-	import java.io.File;
+import java.io.File;
 
 import javax.imageio.ImageIO;
 
-
-	public class BlackAndWhite {
-
-		public static String matToCSVString(Mat m){
-			// Precondition: m is a matrix x*y*1 (gray image)
-			Size size = m.size();
-			StringBuilder stringBuilder = new StringBuilder();
-			byte[] d = new byte[(int) (size.width * size.height)];
-			m.get(0, 0, d);
-		    for (int x = 0; x < size.height; x++) {
-		    	for (int y = 0; y < size.width; y++) {
-		    		if (stringBuilder.length()>0)
-		    			stringBuilder.append(",");
-		    		stringBuilder.append(d[(int) (y * size.height + x)]); 
-		        }
-		    }
-		    return stringBuilder.toString();
+public class BlackAndWhite {
+	public static String matToCSVString(Mat m) {
+		// Precondition: m is a matrix x*y*1 (gray image)
+		Size size = m.size();
+		StringBuilder stringBuilder = new StringBuilder();
+		byte[] d = new byte[(int) (size.width * size.height)];
+		m.get(0, 0, d);
+		for (int x = 0; x < size.height; x++) {
+			for (int y = 0; y < size.width; y++) {
+				if (stringBuilder.length() > 0)
+					stringBuilder.append(",");
+				stringBuilder.append(d[(int) (y * size.height + x)]);
+			}
 		}
+		return stringBuilder.toString();
+	}
+
+	public void run() throws FileNotFoundException, UnsupportedEncodingException {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+		File folderPos = new File("/Users/Delilah/Documents/finalproject/POSV/");
+		File[] listOfFilesPos = folderPos.listFiles();
+
+		File folderNeg = new File("/Users/Delilah/Documents/finalproject/NEG/");
+		File[] listOfFilesNeg = folderNeg.listFiles();
 		
-		public void run() {
+		PrintWriter printwriter = new PrintWriter("/Users/Delilah/Documents/finalproject/test.csv", "UTF-8");
 
-			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		// for (int i = 0; i < 2;i++){//listOfFiles.length; i++){
+		for (int i = 0; i < listOfFilesPos.length; i++) {
+			String picname = (listOfFilesPos[i]).getName(); // filename in input
+															// folder
+			// System.out.println(picname);
+			String PosPath = "/Users/Delilah/Documents/finalproject/POSV/"; // input
+																			// folder
+			String picPathName = PosPath + picname; // pic filename is the
+													// foldername and the
+													// filename
+			// System.out.println(picPathName);
+			String positiveSamples = "N";
+			Mat image = Imgcodecs.imread(picPathName);
+			Mat grayImage = new Mat();
+			Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_RGB2GRAY);
 
-			File folder = new File("/Users/Delilah/Documents/finalproject/testdalal/");
-			File[] listOfFiles = folder.listFiles();
+			Mat smallGrayImage = new Mat();
+			Imgproc.resize(grayImage, smallGrayImage, new Size(59, 100));
+			//System.out.println(positiveSamples + ","
+			//		+ matToCSVString(smallGrayImage)); // adding the string "N"
+														// to matToCSNString
 			
-			File folderBW = new File("/Users/Delilah/Documents/finalproject/testdalal/TEST/BlackAndWhite/");
-			File[] listOfFilesBW = folderBW.listFiles();
-			
- 
+			printwriter.println(positiveSamples + ","
+					+ matToCSVString(smallGrayImage));
+		}
 
-			
-			//for (int i = 0; i < 2;i++){//listOfFiles.length; i++){
-			for (int i = 0; i < listOfFiles.length; i++){
-				 
-				String picname = (listOfFiles[i]).getName(); //filename in input folder
-				//System.out.println(picname);
-				
-				String bwPath = "/Users/Delilah/Documents/finalproject/testdalal/"; //input folder
-			 
-				String picPathName = bwPath + picname; //pic filename is the foldername  and the filename
-				
-				//System.out.println(picPathName);
-				 
-				Mat image = Imgcodecs.imread(picPathName);
-				Mat grayImage = new Mat();
-				Imgproc.cvtColor( image, grayImage, Imgproc.COLOR_RGB2GRAY);
-				
-				Mat smallGrayImage = new Mat ();
-				Imgproc.resize(grayImage, smallGrayImage, new Size(59,100));
-				System.out.println(matToCSVString(smallGrayImage));
-				
-				
-				/*File input = new File(picPathName);
-		        BufferedImage image;
-				try {
-					image = ImageIO.read(input); //read the image
-					
-			         byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-					 Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3); //create mat using the heigh and width of the image
-			         mat.put(0, 0, data);
-			         Mat mat1 = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3); //creates mat1, grayscaled image
-			     	
-			        // mgproc.cvtColor(mat, mat1, Imgproc.COLOR_RGB2HSV); //take mat and changes it into mat1 OF GRAYCOLOR
-			         Imgproc.cvtColor(mat, mat1, Imgproc.COLOR_RGB2GRAY);
-			         
-					 String fileName = listOfFiles[i].getName()+"-bw.jpg";	
-					 //System.out.println("name of output path and file name " + folderBW +"/"+fileName+"-bw.jpg"); //bw.jpg to the filename and print it
-					 
-					 byte[] data1 = new byte[mat1.rows()*mat1.cols()*(int)(mat1.elemSize())];
-			         mat1.get(0, 0, data1);
-			         BufferedImage image1 = new BufferedImage(mat1.cols(), mat1.rows(), 5);
-			         image1.getRaster().setDataElements(0, 0, mat1.cols(), mat1.rows(), data1); //builds the output image 
- 
-			         File ouptut = new File(fileName);
-			        // File ouptut = new File("/Users/Delilah/Documents/finalproject/testdalal/TEST/BlackAndWhite2/"+fileName);
-			         ImageIO.write(image1, "jpg", ouptut); //outputs image 1 writes it to the folder
-					
-					
-					
-					
-					
-					
-					Imgcodecs.imwrite("/Users/Delilah/Documents/finalproject/testdalal/TEST/BlackAndWhite/"+fileName+"-bw.jpg", mat1);
-			
-				} catch (IOException e) {
-					System.out.println("cant read image");
-					e.printStackTrace();
-				}*/
-			}//for all files in the folder
+		for (int i = 0; i < listOfFilesNeg.length; i++) {
+			String picname = (listOfFilesNeg[i]).getName(); // filename in input
+															// folder
+			// System.out.println(picname);
+			String PosPath = "/Users/Delilah/Documents/finalproject/NEG/"; // input
+																			// folder
+			String picPathName = PosPath + picname; // pic filename is the
+													// foldername and the
+													// filename
+			// System.out.println(picPathName);
+			String negativeSamples = "Y";
+			Mat image = Imgcodecs.imread(picPathName);
+			Mat grayImage = new Mat();
+			Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_RGB2GRAY);
 
-			 
+			Mat smallGrayImage = new Mat();
+			Imgproc.resize(grayImage, smallGrayImage, new Size(59, 100));
+			//System.out.println(negativeSamples + ","
+			//		+ matToCSVString(smallGrayImage));
 			
-		}//run
-
- 
-		public static void main (String[] args) {
-			BlackAndWhite fd = new BlackAndWhite();
-			fd.run();
+			printwriter.println(negativeSamples + ","
+					+ matToCSVString(smallGrayImage));
 		}
 	}
 
-
-
-	
-        
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		BlackAndWhite fd = new BlackAndWhite();
+		fd.run();
+	}
+}
